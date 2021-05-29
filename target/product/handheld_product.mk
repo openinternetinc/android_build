e9cd05bf3008fb4cacaac9963d5cc93898026790
g <gWOLF3@protonmail.com>
2021-05-29 14:42:19 -0500

    add some apps

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,6 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
+		# better apps
+		DuckDuckGo \
+		Wireguard \
+		F-droid \
+		Forcastie \
+		# end
     Browser2 \
     Calendar \
     Camera2 \
